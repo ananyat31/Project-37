df14bdb0305e3b6c5862117b70a776f5dd1a4740
ananyat31 <72913055+ananyat31@users.noreply.github.com>
2021-07-21 13:46:54 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,65 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    question.hide();
+    //write code to change the background color here
+    background("yellow")
+    //write code to show a heading for showing the result of Quiz
+    textSize(35);
+    text("Quiz Results:", 350, 50);
+    //call getContestantInfo( ) here
+    Contestant.getPlayerInfo();
+
+    //write condition to check if contestantInfor is not undefined
+    if(allContestants!==undefined){
+      //debugger;
+     var displayAnswers = 230;
+     fill("blue")
+     textSize(25);
+     text("*Note: Contestants who gave the right answers will be highlighted in green color", 90, 230);
+    }
+    //write code to highlight contest who answered correctly
+    for(var plr in allContestants){
+      //debugger;
+      var correctAns = "2";
+      if(correctAns === allContestants[plr].answer)
+        fill("green");
+      else
+       fill("red");
+
+     displayAnswers+= 30;
+     textSize(30)
+     text(allContestants[plr].name+":" + allContestants[plr].answer, 250, displayAnswers);
+    }
+  }
+
+}
